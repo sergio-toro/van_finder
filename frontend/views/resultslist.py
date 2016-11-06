fa8f821e26de59b1b95233bfd76aeb9cb5128d33
Sergio Toro <yiti007@gmail.com>
2016-11-06 23:34:25 +0100

    add viewed status

--- frontend/views/resultslist.py
+++ frontend/views/resultslist.py
@@ -1,5 +1,6 @@
 from finder.models import Result
 from django.views.generic import ListView
+from django.http import JsonResponse
 
 
 class ResultsListView(ListView):
@@ -7,11 +8,37 @@ class ResultsListView(ListView):
     template_name = 'list.html'
     context_object_name = 'results_list'
     paginate_by = 10
-    ordering = '-created'
 
     def get_queryset(self):
         search = self.request.GET.get('search', '')
         if search:
-            return Result.objects.filter(title__search=search)
+            queryset = Result.objects.filter(title__search=search)
         else:
-            return Result.objects.all()
+            queryset = Result.objects.all()
+
+        queryset = queryset.order_by('viewed', '-created', 'pk')
+
+        return queryset
+
+    def post(self, request):
+        result_id = request.POST.get('id', None)
+        action = request.POST.get('action', None)
+        if not result_id or not action:
+            return JsonResponse({"success": False})
+
+        result = Result.objects.get(pk=result_id)
+        if not result:
+            return JsonResponse({"success": False})
+
+        if action == 'viewed':
+            result.viewed = not result.viewed
+            result.save()
+
+        response = {
+            "success": True,
+            "result": {
+                "id": result.id,
+                "viewed": result.viewed,
+            },
+        }
+        return JsonResponse(response)
