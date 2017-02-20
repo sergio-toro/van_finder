2c20d74de1cad209c656d03c6b3cb37a8438f74a
Sergio Toro <yiti007@gmail.com>
2017-02-20 20:43:00 +0100

    cleanup file

--- setup_db.sql
+++ setup_db.sql
@@ -1,7 +1,3 @@
-#sudo apt-get install python-pip python3-dev libpq-dev postgresql postgresql-contrib
-#sudo su - postgres
-#psql
-
 CREATE DATABASE van_finder;
 CREATE USER van_finder WITH PASSWORD '123456';
 
@@ -10,6 +6,3 @@ ALTER ROLE van_finder SET default_transaction_isolation TO 'read committed';
 ALTER ROLE van_finder SET timezone TO 'Europe/Madrid';
 
 GRANT ALL PRIVILEGES ON DATABASE van_finder TO van_finder;
-
-#\q
-#exit
